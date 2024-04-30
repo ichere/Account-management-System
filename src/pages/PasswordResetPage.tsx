16f6866edfe519aef0fff14b939126c1d2c80176
unknown <izuagbalachere@gmail.com>
2024-04-30 17:55:47 +0100

    creation of email templates & password reset pages

---
+++ src/pages/PasswordResetPage.tsx
@@ -0,0 +1,94 @@
+import { Box, Button, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import { AiOutlineLock } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { ChangeInterface, LoginFormValues } from '../interfaces/forms/formProps';
+import ResponsiveAuthWrapper from '../components/ResponsiveAuthWrapper';
+import AuthHeader from '../components/AuthHeader';
+import { CustomInputGroup } from '../components/CustomInputGroup';
+import { COLORS } from '../constants/colors';
+import { LargeBtn } from '../components/LargeBtn';
+
+
+
+const PasswordResetPage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [formValues, setFormValues] = useState<LoginFormValues>({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value }: ChangeInterface = e.target;
+    setFormValues((prev: LoginFormValues) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async () => {
+    setLoading(false);
+  };
+  return (
+    <ResponsiveAuthWrapper  heading={''} subHeading={''}>
+      <Box width={'100%'}>
+        <AuthHeader heading="Password Reset" subHeading="Create a new password" />
+        <Box mt="1.5rem">
+          <CustomInputGroup
+            placeholder={'Password(min 8 character)'}
+            type={'password'}
+            label={'Password'}
+            name="password"
+            borderColor={COLORS.blue}
+            icon={<AiOutlineLock />}
+            value={formValues.password}
+            handleChange={handleChange}
+          />
+        </Box>
+        <Box mt="1.5rem">
+          <CustomInputGroup
+            placeholder={'Password(min 8 character)'}
+            type={'password'}
+            label={'Confirm Password'}
+            name="password2"
+            borderColor={COLORS.blue}
+            icon={<AiOutlineLock />}
+            value={formValues.password}
+            handleChange={handleChange}
+          />
+        </Box>
+        <Box mt="4rem" position="relative">
+          <LargeBtn
+            text={'Proceed'}
+            bg={COLORS.blue}
+            color={COLORS.black}
+            handleSubmit={handleSubmit}
+            loading={loading}
+          />
+        </Box>
+        <Box mt="4rem" position="relative">
+          <Button
+            type="submit"
+            width="100%"
+            height={['4rem', '4rem', '4.5rem']}
+            color={COLORS.black}
+            bg={COLORS.white}
+            borderRadius={'0.8rem'}
+            border={`1px solid ${COLORS.blue}`}
+            fontSize={['1.4rem', '1.4rem', '2rem']}
+            _hover={{
+              bg: COLORS.white,
+              color: COLORS.black,
+            }}
+            onClick={() => navigate('/login')}
+          >
+            Remember your password?{' '}
+            <Text ml=".3rem" color={COLORS.blue} as={'span'}>
+              Login
+            </Text>
+          </Button>
+        </Box>
+      </Box>
+    </ResponsiveAuthWrapper>
+  );
+};
+
+export default PasswordResetPage;
---
+++ src/pages/ResetSuccessPage.tsx
@@ -0,0 +1,60 @@
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import { AiTwotoneLock } from 'react-icons/ai';
+import ResponsiveAuthWrapper from '../components/ResponsiveAuthWrapper';
+import AuthHeader from '../components/AuthHeader';
+import { COLORS } from '../constants/colors';
+import { LargeBtn } from '../components/LargeBtn';
+
+
+const ResetSuccessPage = (): JSX.Element => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleSubmit = async () => {
+    setLoading(false);
+  };
+  return (
+    <ResponsiveAuthWrapper heading={''} subHeading={''}>
+      <Box width={'100%'}>
+        <AuthHeader heading="Forgot Password" />
+        <Flex
+          flexDirection={'column'}
+          justifyContent={'center'}
+          align={'center'}
+          width={'100%'}
+          mt="1.5rem"
+        >
+          <Flex
+            justify={'center'}
+            align="center"
+            w="8rem"
+            h="8rem"
+            bg={'#FEFAE6'}
+            borderRadius={'50%'}
+          >
+            <AiTwotoneLock size={50} color={COLORS.blue} />
+          </Flex>
+          <Box mt="1.5rem" maxW="40rem">
+            <Heading as="h3" textAlign={'center'}>
+              Password Reset Successfully.
+            </Heading>
+            <Text fontSize="1.5rem" mt="1.5rem" textAlign={'center'} color={COLORS.grey}>
+              You have successfully changed your password. Use the button below to login.
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
+      <Box mt="5rem" position="relative">
+        <LargeBtn
+          text={'Proceed'}
+          bg={COLORS.blue}
+          color={COLORS.black}
+          h={['4rem', '4rem', '4.5rem']}
+          handleSubmit={handleSubmit}
+          loading={loading}
+        />
+      </Box>
+    </ResponsiveAuthWrapper>
+  );
+};
+
+export default ResetSuccessPage;
